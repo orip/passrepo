03bd43de1d70c6ef937b64d5ce0b3efd2cbb719a
Ori Peleg <nospam@orip.org>
2012-09-05 12:01:56 +0300

    Added back sample encryption driving code

--- src/com/example/passrepo/PasswordEntryListActivity.java
+++ src/com/example/passrepo/PasswordEntryListActivity.java
@@ -3,12 +3,20 @@ package com.example.passrepo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NavUtils;
-import android.view.MenuItem;
+import android.util.Base64;
+
+import com.example.passrepo.crypto.Encryption;
+import com.example.passrepo.crypto.Encryption.CipherText;
+import com.example.passrepo.crypto.PasswordHasher;
+import com.example.passrepo.crypto.PasswordHasher.ScryptParameters;
+import com.example.passrepo.util.Logger;
+import com.google.common.base.Charsets;
 
 public class PasswordEntryListActivity extends FragmentActivity
         implements PasswordEntryListFragment.Callbacks {
 
+    private static final String TAG = "PasswordEntryListActivity";
+    
     private boolean mTwoPane;
 
     @Override
@@ -40,5 +48,21 @@ public class PasswordEntryListActivity extends FragmentActivity
             detailIntent.putExtra(PasswordEntryDetailFragment.ARG_ITEM_ID, id);
             startActivity(detailIntent);
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        testDriveEncyrption();
+    }
+
+    private void testDriveEncyrption() {
+        ScryptParameters scryptParameters = new ScryptParameters();
+        byte[] key = PasswordHasher.hash("foo", scryptParameters);
+        CipherText encrypted = Encryption.encrypt("What's up man?".getBytes(Charsets.UTF_8), key);
+        String decrypted = new String(Encryption.decrypt(encrypted, key), Charsets.UTF_8);
+        Logger.i(TAG, "derivedKey=%s", Base64.encodeToString(key, Base64.NO_WRAP));
+        Logger.i(TAG, "encrypted=%s", Base64.encodeToString(encrypted.bytes, Base64.NO_WRAP));
+        Logger.i(TAG, "decrypted=%s", decrypted);
     }
 }
