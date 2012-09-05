26f2304488ef5e6613b7e911a93dc1d35b81f5e5
Ori Peleg <nospam@orip.org>
2012-09-05 19:47:30 +0300

    Copying the actual password

--- src/com/example/passrepo/CopyPasswordToClipboardActivity.java
+++ src/com/example/passrepo/CopyPasswordToClipboardActivity.java
@@ -4,16 +4,27 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-import android.os.Bundle;
+import android.content.Intent;
 import android.widget.Toast;
+
+import com.example.passrepo.model.Model;
+import com.example.passrepo.model.PasswordEntry;
+import com.google.common.base.Preconditions;
 
 public class CopyPasswordToClipboardActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        clipboard.setPrimaryClip(ClipData.newPlainText("simple text", "Hello, World!"));
-        Toast.makeText(this, "Copied password to clipboard", Toast.LENGTH_LONG).show();
+    protected void onStart() {
+        super.onStart();
+        Intent intent = getIntent();
+        if (intent != null) {
+            String itemId = Preconditions.checkNotNull(intent.getExtras().getString(Consts.ARG_ITEM_ID));
+            PasswordEntry item = Model.currentModel.getPasswordEntry(itemId);
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            clipboard.setPrimaryClip(ClipData.newPlainText(String.format("password for '%s'", item.title), item.password));
+            Toast.makeText(this, "Copied password to clipboard", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Error copying to clipboard", Toast.LENGTH_LONG).show();
+        }
         finish();
     }
 }
--- src/com/example/passrepo/PasswordEntryDetailFragment.java
+++ src/com/example/passrepo/PasswordEntryDetailFragment.java
@@ -37,10 +37,10 @@ public class PasswordEntryDetailFragment extends PasswordEntryDetailFragmentBase
     public void onResume() {
         super.onResume();
         Logger.i("bla", "detail view onResume");
-        Intent intent = new Intent(getActivity(), CopyPasswordToClipboardActivity.class);
+        Intent intent = new Intent(getActivity(), CopyPasswordToClipboardActivity.class).putExtra(Consts.ARG_ITEM_ID, mItem.id);
         PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(), 0, intent, 0);
         Notification notification = new NotificationCompat.Builder(getActivity())
-                .setContentTitle(String.format("Password entry for '%s'", mItem.title))
+                .setContentTitle(String.format("Copy password for '%s'", mItem.title))
                 .setContentText("Tap to copy password to clipboard").setAutoCancel(false).setPriority(Notification.PRIORITY_HIGH)
                 .setOngoing(true).setContentIntent(pendingIntent).setSubText(String.format("Username: '%s'", mItem.userName))
                 .setSmallIcon(android.R.drawable.ic_input_add)
