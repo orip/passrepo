fd5793b0d1995ffc459557dec0df69cedea1b8e9
Ori Peleg <nospam@orip.org>
2012-09-05 16:31:50 +0300

    GoogleDriveUtil placeholder

---
+++ src/com/example/passrepo/gdrive/GoogleDriveUtil.java
@@ -0,0 +1,13 @@
+package com.example.passrepo.gdrive;
+
+import java.io.File;
+
+public class GoogleDriveUtil {
+    public static void upload(File file) {
+        // TODO
+    }
+    
+    public static void download(File file) {
+        // TODO
+    }
+}
