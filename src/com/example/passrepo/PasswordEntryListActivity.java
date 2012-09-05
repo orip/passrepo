00230b5fd84a47ce30f59f1ca52678b7423ff2ed
Ori Peleg <nospam@orip.org>
2012-09-05 19:18:39 +0300

    Can now change the password

--- src/com/example/passrepo/PasswordEntryListActivity.java
+++ src/com/example/passrepo/PasswordEntryListActivity.java
@@ -1,9 +1,19 @@
 package com.example.passrepo;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.passrepo.crypto.Encryption;
 import com.example.passrepo.crypto.Encryption.CipherText;
@@ -71,6 +81,36 @@ public class PasswordEntryListActivity extends FragmentActivity implements Passw
             Logger.i("TEST", "derivedKey=%s", Base64.encodeToString(key, Base64.NO_WRAP));
             Logger.i("TEST", "encrypted=%s", Base64.encodeToString(encrypted.bytes, Base64.NO_WRAP));
             Logger.i("TEST", "decrypted=%s", decrypted);
+        }
+    }
+
+    private static final int MENU_CHANGE_PASSWORD = 1;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, MENU_CHANGE_PASSWORD, Menu.NONE, R.string.change_password_menu_label).setIcon(
+                android.R.drawable.ic_menu_agenda);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case MENU_CHANGE_PASSWORD:
+            LayoutInflater factory = LayoutInflater.from(this);
+            final View textEntryView = factory.inflate(R.layout.change_password_alert_dialog, null);
+
+            new AlertDialog.Builder(this).setView(textEntryView).setPositiveButton("Update", new OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    String password = ((EditText) textEntryView.findViewById(R.id.password_entry_1)).getText().toString();
+                    Model.currentModel.key = PasswordHasher.hash(password, Model.currentModel.scryptParameters);
+                    IO.saveModel(PasswordEntryListActivity.this);
+                    Toast.makeText(PasswordEntryListActivity.this, "Password updated", Toast.LENGTH_LONG).show();
+                }
+            }).setNegativeButton("Not now", null).setCancelable(true).setOnCancelListener(null).show();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
         }
     }
 }
