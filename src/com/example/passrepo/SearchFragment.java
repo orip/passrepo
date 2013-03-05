acda065be76b66070d101eb78a910abafdb507c6
Dekel Naar <dekel.naar@onavo.com>
2013-03-05 15:58:48 +0200

    changed search fragment location

--- src/com/example/passrepo/SearchFragment.java
+++ src/com/example/passrepo/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.example.passrepo;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
+import android.widget.TextView;
 import com.example.passrepo.events.SearchQueryUpdatedEvent;
 import com.squareup.otto.Bus;
 
@@ -34,6 +36,11 @@ public class SearchFragment extends Fragment implements SearchView.OnQueryTextLi
         SearchView sv = ((SearchView) getActivity().findViewById(R.id.searchView));
         sv.setOnQueryTextListener(this);
         sv.setIconifiedByDefault(false);
+
+        // HACK to change text color
+        int id = sv.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+        TextView textView = (TextView) sv.findViewById(id);
+        textView.setTextColor(Color.WHITE);
     }
 
     public boolean onQueryTextChange(String newText) {
